139b1d147e9571ca97bbc21f57052f0e1ca71cc2
madhurab00 <e20093@eng.pdn.ac.lk>
2026-05-03 10:44:59 +0530

    updated ingestion pipeline to use config file instead of hardcoded paths

--- src/ingestion/ingestion_pipeline.py
+++ src/ingestion/ingestion_pipeline.py
@@ -4,6 +4,8 @@ from src.ingestion.ingest import extract_text_from_pdf
 from src.ingestion.chunk import chunk_text
 from src.ingestion.store import store_chunks_in_db
 
+from src.utils import load_config
+
 def ingest_document(file_path: str, method: str = "fixed"):
     """
     Full ingestion pipeline:
@@ -15,10 +17,14 @@ def ingest_document(file_path: str, method: str = "fixed"):
     documents = extract_text_from_pdf(file_path)
     print(f"Chunking text ...")
     chunks = chunk_text(documents,"overlap")
+    print(f"Storing Chunks in Postgresql database...")
     store_chunks_in_db(chunks)
+    print(f"Finished ingestion for {file_path}")
 
 if __name__ == '__main__':
-    pdf_dir = Path("data/pdfs")
+
+    config = load_config("config/config.yaml")
+    pdf_dir = Path(config["data_root"]) / "pdfs"
     pdf_files = list(pdf_dir.glob("*.pdf"))
     for pdf_path in pdf_files:
         ingest_document(pdf_path)
